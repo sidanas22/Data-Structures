f704645177216586a6f18c7abaf7d55a3adba68d
zarra-e-bayneshan <50417848+zarra-e-bayneshan@users.noreply.github.com>
2019-12-06 20:08:43 +0500

    Added the main file of the project
    We will add header files and such to the project later. This is the initial version of our DSA Project.

---
+++ proj.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+enum state{
+	Match,NotMatch,Star,Dot
+};
+
+//a is without stars and dots, source string
+//b is the string we will compare
+int stringmatch(string a,string b)
+{
+	state currentState = Match;
+	int i=0,j=0;
+	
+	while(1)
+	{
+	
+	switch (currentState)
+	{
+	case Match:
+		if (i==a.length() && j==b.length()){return 1;}
+		if (a[i]==b[j]){i++;j++; currentState=Match;}
+		else if (b[j]=='*'){i++;j++;currentState = Star;} 
+		else if (b[j]=='.'){i++;j++;currentState =  Dot;}
+		else {currentState = NotMatch;}
+		break;
+	case NotMatch:
+		return 0;
+		break;
+	case Star:
+		while(b[j]!=a[i] && i != a.length())
+		{i++;}
+		if (i==a.length() && j==b.length()){return 1;}
+		if (a[i]==b[j]){i++;j++;currentState=Match;}
+		else {currentState = NotMatch;}
+		break;
+	case Dot:
+		if (a[i]==b[j]){i++;j++;currentState=Match;}
+		else if (b[j]=='*'){j++;currentState = Star;}
+		else if (b[j]=='.'){i++;j++;currentState =  Dot;} 
+		else {currentState = NotMatch;} 
+		break;
+	default:
+		break;				
+	}
+	}
+	
+}
+
+
+int main()
+{
+	string line1,line2;
+	cin>>line2;
+	
+	fstream myfile;
+	myfile.open("sample.txt",ios::in);
+	
+	while(!myfile.eof())
+	{myfile>>line1;
+	int result = stringmatch(line1,line2);
+	if (result == 1)
+	{
+		cout<<line1<<endl;
+	}
+	}
+	system("pause");
+	return 0;
+}
