e188ecfc572872a6af210ff5b23c3fcd289a07b6
zarra-e-bayneshan <50417848+zarra-e-bayneshan@users.noreply.github.com>
2019-12-23 19:27:51 +0500

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -1,101 +1,286 @@
-#include <iostream>
+#include<iostream>
 #include <fstream>
+#include<string>
 using namespace std;
+
 
 enum state{
 	Match,NotMatch,Star,Dot
 };
 
-//a is without stars and dots, source string
-//b is the string we will compare
-int stringmatch(string a,string b)
-{
-	state currentState = Match;
-	int i=0,j=0;
+
+
+
+class node{
+	public:
+		string data;
+		node* next;
+	node(string new_s,node *n = NULL )
+	{
+		data = new_s;
+		
+		next = n;
+	}
+
+};
+
+class SLL{
+	public:
+	node* head;
+	node* tail;
 	
-	while(1)
+	SLL(){
+		head=NULL;
+		tail=NULL;
+	}
+	
+	void add_to_tail(string new_s)
 	{
-	switch (currentState)
-	{
-	case Match:
-		if (i==a.length() && j==b.length()){return 1;}
-		if (a[i]==b[j]){i++;j++; currentState=Match;}
-		else if (b[j]=='*'){i++;j++;currentState = Star;} 
-		else if (b[j]=='.'){i++;j++;currentState =  Dot;}
-		else {currentState = NotMatch;}
-		break;
-	case NotMatch:
-		return 0;
-		break;
-	case Star:
-		while(1)
+		if (tail==NULL)
 		{
-			if (j==b.length() && i < a.length()){return 1;}
-			
-			while(b[j]!=a[i] && i <= a.length()-1)
-			{i++;}
-			
-			if (i==a.length()-1){break;}
-			
-			if (b[j+1]=='*'){break;}
-			if (b[j+1]=='.'){break;}
-			
-			
-			
-			while (a[i+1]!=b[j+1] && i<=a.length())
-			{i++;}
-			
-			if (i>a.length()){break;}
-			
-			int  starflag=0,lenb=0,x;
-			
-			for (x=j;x<b.length();x++)
+			head=tail=new node(new_s);
+		}
+		else
+		{
+			tail->next= new node(new_s);
+			tail=tail->next;	
+		}
+	}
+	
+	
+	void swap( int a,int  b)
+	{
+		if (a==b){return;}
+		else{
+	//		if (a==1){swap_head(a,b);return;}
+	//		if (b==1){swap_head(b,a);return;}
+			a--;b--;
+			int count=1;
+			node *temp1=NULL,*temp2=NULL;
+			node *temp=head;
+			while(temp!=NULL)
 			{
-				if (b[x]=='*'){starflag = 1; break;}
-				else{lenb++;}
+				if(a==count){temp1=temp;}
+				else if(b==count){temp2=temp;}
+				count++;
+				temp=temp->next;	
 			}
-			int lena = a.length()-i-1;
-			if ((starflag==1 && a[x+1]==b[j] && a[x+1]==b[j+1]) || (starflag==0 && lena>lenb)){i++; continue;}
-//			if (a[i+1]==b[j+1] && starflag==0 && lena==lenb)
-			else
-			{break;}
+			
+			if(temp1==NULL || temp2==NULL){cout<<"position to swap nodes is out of bound"<<endl;return;}
+			
+			temp= temp1->next;
+			temp1->next=temp2->next;
+			temp2->next=temp;
+			
+			temp1=temp1->next;
+			temp2=temp2->next;
+			
+			temp= temp1->next;
+			temp1->next=temp2->next;
+			temp2->next=temp;
 		}
 		
-		if (i==a.length() && j==b.length()){return 1;}
-		if (a[i]==b[j]){i++;j++;currentState=Match;}
-		else {currentState = NotMatch;}
-		break;
-		
-	case Dot:
-		if (i==a.length() && j==b.length()){return 1;}
-		if (a[i]==b[j]){i++;j++;currentState=Match;}
-		else if (b[j]=='*'){j++;currentState = Star;}
-		else if (b[j]=='.'){i++;j++;currentState =  Dot;} 
-		else {currentState = NotMatch;} 
-		break;
-	default:
-		break;				
-	}
 	}
 	
-}
+	void print()
+	{
+		for(node *temp = head;temp!=NULL;temp = temp->next)
+		{
+			cout<<temp->data<<endl;
+		}
+		cout<<"\n\n\n________ENDING___________";
+		
+	}
+	
+	
+};
+
+
+class Finite_state_machine
+{
+	private:
+		string pattern;
+		fstream myfile;
+		string filename;
+		SLL wordlist;
+	
+	public:
+		
+	
+	void get_filename(string file_name)
+	{
+		this->filename = file_name;
+		filename = filename+".txt";
+	}
+	
+	
+	void regex(string pattern1)
+	{
+		string word_from_file;
+		this->pattern = pattern1;
+		
+		
+		//FILING WORK
+		
+		
+		myfile.open(filename.c_str(),ios::in);
+		
+		if(myfile.is_open()==0)
+		{
+			cout<<"FILE COULD NOT BE OPPENED\n";
+			exit(1);
+		}
+		
+		
+		while(!myfile.eof())
+		{
+			getline(myfile,word_from_file);
+			int result = stringmatch(word_from_file,pattern);
+			if (result == 1)
+			{
+			wordlist.add_to_tail(word_from_file);
+			}
+		}
+		myfile.close();
+		wordlist.print();
+	}
+		
+	int stringmatch(string a,string b){
+		
+		
+			state currentState = Match;
+			int i=0,j=0;
+			
+			while(1)
+			{
+			switch (currentState)
+			{
+			
+			
+			case Match:
+				if (i==a.length() && j==b.length()){return 1;}
+				if (a[i]==b[j]){i++;j++; currentState=Match;}
+				else if (b[j]=='*'){j++;currentState = Star;} 
+				else if (b[j]=='.'){i++;j++;currentState =  Dot;}
+				else {currentState = NotMatch;}
+				break;
+			
+			
+			case NotMatch:
+				return 0;
+				break;
+			
+			
+			case Star:
+				
+				
+				while(1)
+				{
+					if (j>b.length()-1 && i <= a.length()){return 1;}
+					
+					while(b[j]!=a[i] && i <= a.length()-1)
+					{i++;}
+					
+					if (i==a.length()-1 || b[j+1]=='*' || b[j+1]=='.'){break;}
+					
+					while (a[i+1]!=b[j+1] && i<=a.length())
+					{i++;}
+					
+					if (i>a.length()){break;}
+					
+					int  starflag=0,lenb=0,x;
+					
+					for (x=j;x<b.length();x++)
+					{
+						if (b[x]=='*'){starflag = 1; break;}
+						else{lenb++;}
+					}
+					int lena = a.length()-i-1;
+					if ((starflag==1 && a[x+1]==b[j] && a[x+1]==b[j+1]) || (starflag==0 && lena>lenb)){i++; continue;}
+					else
+					{break;}
+				}
+				
+				if (i==a.length() && j==b.length()){return 1;}
+				if (a[i]==b[j]){i++;j++;currentState=Match;}
+				else {currentState = NotMatch;}
+				break;
+				
+			
+			
+			case Dot:
+				if (i==a.length() && j==b.length()){return 1;}
+				if (a[i]==b[j]){i++;j++;currentState=Match;}
+				else if (b[j]=='*'){j++;currentState = Star;}
+				else if (b[j]=='.'){i++;j++;currentState =  Dot;} 
+				else {currentState = NotMatch;} 
+				break;
+			
+				default:
+				break;				
+			}
+			
+		}
+	}
+};
+
 
 int main()
 {
-	string line1,line2;
-	cin>>line2;
-	
-	fstream myfile;
-	myfile.open("sample.txt",ios::in);
-	
-	while(!myfile.eof())
+	int num,flag=1; 
+	string pattern;
+	string answer;
+	cout<<"\nWelcome!\n";
+	while(flag==1)
 	{
-		myfile>>line1;
-		int result = stringmatch(line1,line2);
-		if (result == 1)
-		{
-			cout<<line1<<endl;
-		}
+	
+	cout<<"Please enter the option no. ('1' or '2'') you want to search the pattern from:\n";
+	cout<<"1. Names\n2. English Dictionary\n";
+	cin>>num;
+	
+	Finite_state_machine fsm;
+	
+	if(num==1)
+	{
+		fsm.get_filename("names");
+//		system("pause");
+//		system("CLS");	
 	}
+	
+	
+	else if(num==2)
+	{
+		fsm.get_filename("english");
+//		system("pause");
+//		system("CLS");
+	}
+	else
+	{
+		std::cout<<"Error! Please enter valid option!";
+//		system("pause");
+//		system("CLS");
+		flag=1;
+		continue;
+	}
+	cout<<"\nPlease enter the string pattern:\n";
+	getline(cin,pattern);
+	fsm.regex(pattern);
+	system("pause");
+	system("CLS");
+    cout<<"\nDo you want to search again ? (yes/no)\n";
+	cin>>answer;
+	if(answer=="yes")
+	{
+		flag = 1;
+	}
+	else if(answer=="no")
+	{
+		flag = 0;
+	}	
+	
+	
+}
+
+		
 	return 0;
 }
